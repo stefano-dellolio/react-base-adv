3e1acc885577303a81031a34e97278ca753dcb44
Stefano <stefano.dellolioprada@ottogroup.com>
2023-11-15 00:03:29 +0100

    Component compound listo

---
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -0,0 +1,25 @@
+import { createContext } from 'react';
+import { useProduct } from '../hooks/useProduct';
+import styles from '../styles/styles.module.css';
+import { ProductCardProps, ProductContextProps } from '../interfaces/interfaces';
+
+export const ProductContext = createContext({} as ProductContextProps);
+const { Provider } = ProductContext;
+
+export const ProductCard = ({ children, product }: ProductCardProps) => {
+
+    const { counter, increaseBy } = useProduct();
+
+    return (
+        <Provider value={{
+            counter,
+            increaseBy,
+            product,
+        }}>
+
+            <div className={ styles.productCard }>
+                { children }
+            </div>
+        </Provider>  
+    )
+}
