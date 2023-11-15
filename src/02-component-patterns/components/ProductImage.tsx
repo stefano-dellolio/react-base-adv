e4440d554d2d3f083cfdd80d362a36997bd74e38
Stefano <stefano.dellolioprada@ottogroup.com>
2023-11-15 01:31:33 +0100

    Component Extend Styles listo

--- src/02-component-patterns/components/ProductImage.tsx
+++ src/02-component-patterns/components/ProductImage.tsx
@@ -1,9 +1,15 @@
-import { useContext } from 'react';
+import { CSSProperties, useContext } from 'react';
 import { ProductContext } from './ProductCard';
 import noImage from '../assets/no-image.jpg';
 import styles from '../styles/styles.module.css';
 
-export const ProductImage = ({ img = '' }) => {
+export interface Props {
+    img?: string;
+    className?: string;
+    style?: CSSProperties;
+}
+
+export const ProductImage = ({ img = '', className, style }: Props ) => {
     const { product } = useContext( ProductContext );
     let imgToShow: string;
     if (img) {
@@ -14,6 +20,11 @@ export const ProductImage = ({ img = '' }) => {
         imgToShow = noImage;
     }
     return (
-        <img className={ styles.productImg } src={ imgToShow } alt="Product" />
+        <img
+            className={ `${styles.productImg} ${ className }` }
+            style={ style }
+            src={ imgToShow }
+            alt="Product"
+        />
     )
 }
