31536746b63c1b6d20632bd28c84c43bb03df73d
Stefano <stefano.dellolioprada@ottogroup.com>
2023-11-20 12:11:16 +0100

    Control props alt

--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -4,18 +4,19 @@ import { Product, ProductInCart } from '../interfaces/interfaces';
 export const useShoppingCart = () => {
     const [ shoppingCart, setShoppingCart ] = useState<{ [ key: string ]: ProductInCart }>({});
     
-    const onProductCountChange = ( { count, product }: { count: number, product: Product } ) => {
+    const onProductCountChange = ({ count, product }: { count: number, product: Product }) => {
+
         setShoppingCart( oldShoppingCart => {
-            const productInCart: ProductInCart = oldShoppingCart[ product.id ] || { ...product, count: 0 };
-            if( Math.max( productInCart.count + count, 0 ) > 0 ) {
-                productInCart.count += count;
-                return {
-                    ...oldShoppingCart,
-                    [ product.id ]: productInCart,
-                }
+
+            if( count === 0 ) {
+                const { [ product.id ]: toDelete, ...rest } = shoppingCart;
+                return rest;
             }
-            const { [ product.id ]: toDelete, ...rest } = oldShoppingCart;
-            return rest;
+    
+            return {
+                ...oldShoppingCart,
+                [ product.id ]: { ...product, count }
+            }
         })
     }
 
